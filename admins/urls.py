b99598eac516d6a328ad150faccdf9780dace833
PKI <pkiprogram@mail.ru>
2021-12-10 20:31:57 +0300

    Homework lesson-8

---
+++ admins/urls.py
@@ -0,0 +1,23 @@
+
+from django.urls import path
+
+from admins.views import IndexTemplateView, UserCreateView, UserUpdateView, UserDeleteView, UserListView, \
+    CategoryListView, CategoryDeleteView, CategoryUpdateView, ProductListView, CategoryCreateView
+
+app_name = 'admins'
+urlpatterns = [
+
+    path('', IndexTemplateView.as_view(),name='index'),
+    path('users/', UserListView.as_view(),name='admin_users'),
+    path('users-create/', UserCreateView.as_view(),name='admin_users_create'),
+    path('users-update/<int:pk>', UserUpdateView.as_view(),name='admin_users_update'),
+    path('users-delete/<int:pk>', UserDeleteView.as_view(),name='admin_users_delete'),
+
+    path('category/', CategoryListView.as_view(), name='admin_category'),
+    path('category/create/', CategoryCreateView.as_view(), name='admin_category_create'),
+    path('category-delete/<int:pk>/', CategoryDeleteView.as_view(), name='admin_category_delete'),
+    path('category-update/<int:pk>/', CategoryUpdateView.as_view(), name='admin_category_update'),
+    # path('category-detail/<int:pk>/', CategoryDetailView.as_view(), name='admin_category_detail'),
+
+    path('product/', ProductListView.as_view(), name='admin_product'),
+]
